1efb3a8a12fb2c1ee756fd8533009a908a73afe7
Wagão - ( Wagner Goulart ) <wcfx.dev@gmail.com>
2022-05-18 03:29:00 -0300

    feat: get products from database

--- src/components/ShoppingList/index.tsx
+++ src/components/ShoppingList/index.tsx
@@ -1,18 +1,32 @@
-import React, { useState } from 'react';
-import { FlatList } from 'react-native';
-
-import { styles } from './styles';
-import { Product, ProductProps } from '../Product';
-
-import { shoppingListExample } from '../../utils/shopping.list.data';
+import React, { useEffect, useState } from "react";
+import { FlatList } from "react-native";
+import firestore from "@react-native-firebase/firestore";
+import { styles } from "./styles";
+import { Product, ProductProps } from "../Product";
 
 export function ShoppingList() {
-  const [products, setProducts] = useState<ProductProps[]>(shoppingListExample);
+  const [products, setProducts] = useState<ProductProps[]>();
+
+  useEffect(() => {
+    firestore()
+      .collection("products")
+      .get()
+      .then((res) => {
+        const data = res.docs.map((doc) => {
+          return {
+            id: doc.id,
+            ...doc.data(),
+          };
+        }) as ProductProps[];
+        setProducts(data);
+      })
+      .catch((error) => console.log(error));
+  }, []);
 
   return (
     <FlatList
       data={products}
-      keyExtractor={item => item.id}
+      keyExtractor={(item) => item.id}
       renderItem={({ item }) => <Product data={item} />}
       showsVerticalScrollIndicator={false}
       style={styles.list}
